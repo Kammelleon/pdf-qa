3c02efb0fce909c1de3586ba3ce0138d17e70981
Kamil Bugla <kamil_bugla@rewsoft.pl>
2025-05-13 18:10:54 +0200

    Deleted baseURL param

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import { API_BASE_URL, DEFAULT_TIMEOUT } from './settings';
+import { DEFAULT_TIMEOUT } from './settings';
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
   timeout: DEFAULT_TIMEOUT,
   headers: {
     'Content-Type': 'application/json',
